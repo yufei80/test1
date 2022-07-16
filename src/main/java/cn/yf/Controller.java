8d26f72478cbe370d1fc9e6d0ca02905633cb81c
Wyf <1561294236@qq.com>
2022-07-16 18:56:03 +0800

    dev第一次提交

--- src/main/java/cn/yf/Controller.java
+++ src/main/java/cn/yf/Controller.java
@@ -2,6 +2,6 @@ package cn.yf;
 
 public class Controller {
     public static void main(String[] args) {
-        System.out.println("aaa ");
+        System.out.println("aaa 111111111111111");
     }
 }
